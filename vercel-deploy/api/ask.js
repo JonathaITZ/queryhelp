304a1f299245e3bc9ef4d5d89ef4e7e209d0ac30
Jonatha Dantas <jonatha.dantas@gmail.com>
2026-09-01 12:30:29 -0300

    fix(vercel): clean package.json and align handler signature - by Jonatha Dantas

--- vercel-deploy/api/ask.js
+++ vercel-deploy/api/ask.js
@@ -16,7 +16,7 @@ function checkRateLimit(ip) {
   return true;
 }
 
-module.exports = async (req, res) => {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
